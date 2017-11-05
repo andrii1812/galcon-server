994ddaa574de0d313fb3cf5ae33c6d58b39a463f
AnnaSokolovska <anetkanfetka@gmail.com>
2017-11-05 22:25:49 +0200

    Adds PlanetUpdate class

---
+++ src/Core/PlanetUpdate.cs
@@ -0,0 +1,16 @@
+﻿namespace GalconServer.Core
+{
+    public class PlanetUpdate
+    {
+        public int ID { get; set; }
+        public double Population { get; set; }
+        public int Owner { get; set; }
+
+        public PlanetUpdate(int id, double population, int owner)
+        {
+            ID = id;
+            Population = population;
+            Owner = owner;
+        }
+    }
+}
