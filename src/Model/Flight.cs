8aab8adc9f42392cdf74711bf3cd4dec03957da7
ABartkoviak <andriy.bartkovyak@hotmail.com>
2017-11-28 01:38:53 +0200

    Fix for issue #14 (difference between protocol description and server implementation in TickUpdate message)

--- src/Model/Flight.cs
+++ src/Model/Flight.cs
@@ -6,13 +6,17 @@ using System.Threading.Tasks;
 namespace GalconServer.Model
 {
     using System.Threading;
+    using Newtonsoft.Json;
 
     public class Flight
     {
         private int _currentPosition;
         public int ID { get; private set; }
+        [JsonProperty("owner")]
         public int OwnerID { get; private set; }
+        [JsonProperty("from")]
         public int FromPlanet { get; private set; }
+        [JsonProperty("to")]
         public int ToPlanet { get; private set; }
         public int Population { get; private set; }
         public int TotalPath { get; private set; }
